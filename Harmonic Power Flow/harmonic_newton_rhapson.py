707a8cddd6a513e23f1d71723ae9764612edda99
Pascal Weigmann <p.weigmann@posteo.de>
2020-04-22 15:11:03 +0200

    hcne v0.2

--- Harmonic Power Flow/harmonic_newton_rhapson.py
+++ Harmonic Power Flow/harmonic_newton_rhapson.py
@@ -4,9 +4,11 @@
 # Author: Pascal Weigmann, p.weigmann@posteo.de
 
 import numpy as np
+import pandas as pd
 from scipy.sparse.linalg import *
 
 pu_factor = 400  # as in PyPSA example
+
 
 # buses and there initial state
 bus0 = {"type": "slack", "P": 100, "Q": 0, "V": 1, "theta": 0}  # gen, slack bus
@@ -96,16 +98,44 @@ bus0.update({"V3": 0.1, "V5": 0.1, "theta3": 0, "theta5": 0})
 bus1.update({"V3": 0.1, "V5": 0.1, "theta3": 0, "theta5": 0})
 bus2.update({"V3": 0.1, "V5": 0.1, "theta3": 0, "theta5": 0})
 
-# grid admittance matrix, harmonic frequencies, scaled linearly
-Y_3 = np.array([[y01+y20, -y01, -y20],
-                [-y01, y12+y01, -y12],
-                [-y12, -y20, y12+y20]])/3
+# line impedance scaled linearly (Fuchs)
+line0.update({"x3": line0["x"]*3, "x5": line0["x"]*5})
+line1.update({"x3": line1["x"]*3, "x5": line1["x"]*5})
+line2.update({"x3": line2["x"]*3, "x5": line2["x"]*5})
 
-Y_5 = np.array([[y01+y20, -y01, -y20],
-                [-y01, y12+y01, -y12],
-                [-y12, -y20, y12+y20]])/5
+# line admittances, harmonic extension
+y01_3 = 1/(line0["R"]/pu_factor + 1j*line0["x3"]/pu_factor)
+y12_3 = 1/(line1["R"]/pu_factor + 1j*line1["x3"]/pu_factor)
+y20_3 = 1/(line2["R"]/pu_factor + 1j*line2["x3"]/pu_factor)
 
-# Norton parameter, nonlinear load (from almeida)
+y01_5 = 1/(line0["R"]/pu_factor + 1j*line0["x5"]/pu_factor)
+y12_5 = 1/(line1["R"]/pu_factor + 1j*line1["x5"]/pu_factor)
+y20_5 = 1/(line2["R"]/pu_factor + 1j*line2["x5"]/pu_factor)
+
+# grid admittance matrix, harmonic frequencies, scaled linearly (probably wrong)
+#Y_3 = np.array([[y01+y20, -y01, -y20],
+#                [-y01, y12+y01, -y12],
+#                [-y12, -y20, y12+y20]])/3
+
+# grid admittance matrix, harmonic frequencies using harmonic line impedances
+# do I need to add shunt admittances for linear buses at harmonic frequencies?
+Y_3 = np.array([[y01_3+y20_3, -y01_3, -y20_3],
+                [-y01_3, y12_3+y01_3, -y12_3],
+                [-y12_3, -y20_3, y12_3+y20_3]])
+Y_5 = np.array([[y01_5+y20_5, -y01_5, -y20_5],
+                [-y01_5, y12_5+y01_5, -y12_5],
+                [-y12_5, -y20_5, y12_5+y20_5]])
+
+# voltage vectors at harmonic frequencies
+V3 = np.array([bus0["V3"] * np.exp(1j * bus0["theta3"]),
+               bus1["V3"] * np.exp(1j * bus1["theta3"]),
+               bus2["V3"] * np.exp(1j * bus2["theta3"])])
+V5 = np.array([bus0["V5"] * np.exp(1j * bus0["theta5"]),
+               bus1["V5"] * np.exp(1j * bus1["theta5"]),
+               bus2["V5"] * np.exp(1j * bus2["theta5"])])
+
+
+# Norton parameter, nonlinear load at bus2 (from almeida)
 Y_N = np.array([
     [-0.79-0.981j, 6.065+8.387j, -38.4-25.34j],
     [-1.216-0.982j, -1.068+5.375j, -2.724-5.45j],
@@ -127,8 +157,34 @@ def current_inj(V):
     return I_inj
 
 
-I_inj = current_inj(np.array([bus2['V'], bus0['V3'], bus0['V5']]))
+I_inj = current_inj(np.array([bus2['V'], bus2['V3'], bus2['V5']]))
 
-# mismatch vector, currents
-dI_f_bus2 = I_inj[0] 
+# update vector
+# with fundamental voltages from power flow and initial harmonic values
+U = np.array([bus1["theta"], bus1["V"], bus2["theta"], bus2["V"],
+              bus0["theta3"], bus0["V3"], bus1["theta3"], bus1["V3"],
+              bus2["theta3"], bus2["V3"], bus0["theta5"], bus0["V5"],
+              bus1["theta5"], bus1["V5"], bus2["theta5"], bus2["V5"],
+              ])
 
+# current mismatch for nonlinear buses
+# (fuchs divides all of these into real and imaginary parts)
+I_lines_f = Y_f.dot(V)
+I_lines_3 = Y_3.dot(V3)
+I_lines_5 = Y_5.dot(V5)
+
+dI_bus2_f = I_inj[0] + I_lines_f[1] + I_lines_f[2]
+dI_bus2_3 = I_inj[1] + I_lines_3[2] + I_lines_3[1]
+dI_bus2_5 = I_inj[2] + I_lines_5[2] + I_lines_5[1]
+
+# harmonic current mismatch for all buses
+dI_bus0_3 = I_lines_3[0] + I_lines_3[2]
+dI_bus0_5 = I_lines_5[0] + I_lines_5[2]
+dI_bus1_3 = I_lines_3[0] + I_lines_3[1]
+dI_bus1_5 = I_lines_5[0] + I_lines_5[1]
+# these are all zero initially, maybe wrong?
+
+# power mismatch (calculate power by currents, compare to given power)
+I_inj_f_abs = 0  # magnitute
+I_inj_f_delta = 0  # phase
+P_bus2_f = I_inj_f_abs*bus2["V"]*np.cos(I_inj_f_delta - bus2["theta"])
