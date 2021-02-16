971924ccf7ffaf2adaa60ea3402b365fc42ef319
Pascal Weigmann <p.weigmann@posteo.de>
2021-02-16 11:42:19 +0100

    added fundamental only simulation finished uncoupled NE calculation

--- Circuit Simulation/NE_from_sim.py
+++ Circuit Simulation/NE_from_sim.py
@@ -1,4 +1,4 @@
-# Importing Simulink simulation results from .mat file and calculating the
+# Importing SimuLink simulation results from .mat file and calculating the
 # corresponding coupled and uncoupled Norton equivalents.
 
 # needs exactly 2 levels of variable simulation parameters
@@ -10,49 +10,82 @@ from scipy.io import loadmat
 
 # import from .mat file
 data = loadmat('circuit_sim.mat', squeeze_me=True, struct_as_record=False)
+df = data["all"].results_f  # fundamental simulation results
+dh = data["all"].results_h  # harmonic simulation results
 
 # convert to multi-index DataFrame
-# TODO: enable single measurement support
-supply_frequencies = []
-for n in data["results"]:
-    supply_frequencies.append(n[0].f_h)
+# TODO: enable single measurement support or raise error if only one measurement
+supply_f = []
+for n in dh:
+    supply_f.append(n[0].f_h)
 
-supply_voltages = []
-for m in data["results"][0, :]:
-    supply_voltages.append(m.V_m_h)
+supply_v_m = []
+for m in dh[0, :]:
+    supply_v_m.append(m.V_m_h)
 
-iterables = [supply_voltages, supply_frequencies]
-multi_idx = pd.MultiIndex.from_product(iterables,
-                                       names=['supply_v_h', 'supply_f_h'])
-spectrum = pd.Int64Index(data["results"][0, 0].H.astype(int),
-                         name="Frequency", dtype="int64")
+# this will only work for constant harmonic supply voltage angles
+supply_v_a = [dh[0, 0].V_a_h]
+
+iterables = [supply_v_m, supply_f, supply_v_a]
+multi_idx = pd.MultiIndex.from_product(iterables, names=['supply_v_m',
+                                                         'supply_f',
+                                                         'supply_v_a'])
+spectrum = pd.Int64Index(dh[0, 0].H.astype(int), name="Frequency",
+                         dtype="int64")
 
 # initialize multi-index DataFrame
-I_inj = pd.DataFrame(np.zeros((len(supply_frequencies)*len(supply_voltages),
-                               len(data["results"][0, 0].H))),
-                     index=multi_idx, columns=spectrum)
+I_inj_complete = pd.DataFrame(np.zeros((len(supply_f)*len(supply_v_m),
+                                        len(dh[0, 0].H))), index=multi_idx,
+                              columns=spectrum)
 
-# iterate through measurements
-for i in data["results"]:
+# iterate through harmonic measurements
+for i in dh:
     for j in i:
-        # write I_inj of corresponding voltage to df
-        I_inj.loc[j.V_m_h, j.f_h] = j.I_inj*np.exp(1j*j.I_inj_phase)
+        # write I_inj of corresponding voltage to DataFrame
+        I_inj_complete.loc[(j.V_m_h, j.f_h, j.V_a_h)] = \
+            j.I_inj*np.exp(1j*j.I_inj_phase)
 
-# final df, only at uneven harmonic frequencies
-I_inj_h = I_inj.loc(axis=1)[50::data["results"][0, 0].cycles*2]
+# iterate through fundamental measurements
+for s in df:
+    # write I_inj of corresponding voltage to DataFrame, f_fund always 50 Hz
+    I_inj_complete.loc[(s.V_m_f, 50, s.V_a_f)] = \
+        s.I_inj*np.exp(1j*s.I_inj_phase)
+
+# final I_inj for all measurements, no inter-harmonics
+I_inj = I_inj_complete.loc(axis=1)[50::dh[0, 0].cycles*2]
 
 
 # calculate Norton Equivalent parameters
 # uncoupled (see Thunberg.1999), 2*n measurements (of 1 freq.) for n harmonics
-# first step: build difference between the two "measurements", without fund
-V_m1 = data["results"][0, 0].V_m_h
-V_m2 = data["results"][0, 1].V_m_h
-dI = (I_inj_h.loc[V_m2] - I_inj_h.loc[V_m1]).drop(50, axis=1)
-# uncoupled: only diagonal elements needed
-# voltage difference is constant wrt frequency, Norton admittance:
-Y_N_uc = np.diag(dI)/(V_m1 - V_m2)
-# Norton current source:
-I_N_uc = Y_N_uc*V_m1 + I_inj_h.loc[(V_m1, 150), 150:]
+# build difference between the two measurements, without fund, m2 - m1
+dI_h = (I_inj.loc[dh[0, 1].V_m_h] - I_inj.loc[dh[0, 0].V_m_h]).drop(
+    50, axis=1)
+# voltage difference is constant wrt frequency
+V_h_m1 = dh[0, 0].V_m_h*np.exp(1j*dh[0, 0].V_a_h*np.pi/180)
+V_h_m2 = dh[0, 1].V_m_h*np.exp(1j*dh[0, 1].V_a_h*np.pi/180)
+# only diagonal elements needed for harmonic frequencies
+# Norton admittance, harmonic
+Y_N_h = pd.Series(np.diag(dI_h)/(V_h_m1 - V_h_m2), index=supply_f)
+Y_N_h.rename_axis(index="Frequency", inplace=True)
+# Norton current source, harmonic:
+I_N_h = Y_N_h*V_h_m1 + I_inj.loc[(dh[0, 0].V_m_h, 150), 150:]
+
+
+# build difference between the two measurements, only fund, m2 - m1
+dI_f = I_inj.loc[(df[1].V_m_f, 50, df[1].V_a_f)] - \
+       I_inj.loc[(df[0].V_m_f, 50, df[0].V_a_f)]
+# voltage difference
+V_f_m1 = df[0].V_m_f*np.exp(1j*df[0].V_a_f*np.pi/180)
+V_f_m2 = df[1].V_m_f*np.exp(1j*df[1].V_a_f*np.pi/180)
+# only fund injection needed for fundamental frequency
+Y_N_f = dI_f[[50]]/(V_f_m1 - V_f_m2)  # Norton admittance, fundamental
+# Norton current source, fundamental:
+I_N_f = Y_N_f*V_f_m1 + I_inj.loc[(df[0].V_m_f, 50, df[0].V_a_f), [50]]
+
+# Final Norton parameters, uncoupled model:
+I_N_uc = I_N_f.append(I_N_h.loc[30])
+Y_N_uc = Y_N_f.append(Y_N_h)
+
 
 # coupled (see Almeida.2010), n+1 measurements (of all freq.) for n harmonics
 
--- Circuit Simulation/sim_FFT.m
+++ Circuit Simulation/sim_FFT.m
@@ -17,7 +17,7 @@ h_max = 550;   % highest harmonic simulated, min = 150
 % fundamental voltage source
 f = 50;  % fundamental frequency
 Va = 230;  % fundamental voltage magnitude
-Initialph_f = 0;  % fundamental voltage phase, [degree]
+Initialph_f_range = [0, 10];  % fundamental voltage phase, [degree]
 
 % harmonic voltage source (variable operating conditions)
 supply_harmonics = 50*(3:2:h_max/f);  % harmonic frequency range
@@ -29,23 +29,109 @@ Initialph_h = 30;  % harmonic voltage phase, [degree]
 t_start = 0.06;  
 % number of periods analyzed
 % also defines interharmonic frequency resolution
-cycles = 2;  
+cycles = 2;
 
-results = struct;
+% deducted simulation parameters
+t_end = t_start + cycles/f - T;
+time_complete = (0:T:t);
+time = (t_start : T : t_end);
+L = length(time);  % time-series length
+H = (0:(L/2))/L/T;  % spectrum
 
+results_f = struct;
+results_h = struct;
+
+% simulate with only fundamental source, vary initial phase
+for k = (1:length(Initialph_f_range))
+    Initialph_f = Initialph_f_range(k);
+    Vh = 0;
+    fh = 0;
+    
+    % execute circuit simulation
+    sim("SMPS")
+
+    % Simulation output
+    Is_complete = S_scope.signals(1).values(1:end);
+    Is = S_scope.signals(1).values(int32(t_start/T+1):int32(t_end/T+1));
+    Vs_complete = S_scope.signals(2).values(1:end);
+    Vs = S_scope.signals(2).values(int32(t_start/T+1):int32(t_end/T+1));
+
+    % FFT current injections source
+    ft_i = fft(Is);
+    % two-sided spectrum. abs is magnitute of frequency
+    P2_i = abs(ft_i/L);  
+    I_inj = P2_i(1:L/2+1);  % single sided spectrum
+    % double to compensate cutting in half
+    I_inj(2:end-1) = 2*I_inj(2:end-1);
+    % phase somehow is shifted by -90 degree (bc of inductive element?)
+    I_inj_phase = angle(ft_i(1:L/2+1))+pi/2;
+
+    % FFT voltage source
+    ft_v = fft(Vs);
+    % two-sided spectrum. abs is magnitute of frequency
+    P2_v = abs(ft_v/L);  
+    Vs_spec = P2_v(1:L/2+1);  % single sided spectrum
+    % double to compensate cutting in half
+    Vs_spec(2:end-1) = 2*Vs_spec(2:end-1);
+    % phase somehow is shifted by -90 degree and interharmonics wrong
+    Vs_phase = angle(ft_v(1:L/2+1))+pi/2;
+
+    % save paramters and results of fundamental sims in struct
+    results_f(k, 1).V_m_f = Va;
+    results_f(k, 1).V_m_h = Vh;
+    results_f(k, 1).V_a_f = Initialph_f;
+    results_f(k, 1).V_a_h = Initialph_h;
+    results_f(k, 1).f_h = fh;
+    results_f(k, 1).H = H(1:int32(cycles*h_max/f+1));
+    results_f(k, 1).I_inj = I_inj(1:int32(cycles*h_max/f+1));
+    results_f(k, 1).I_inj_phase = I_inj_phase(1:int32(cycles*h_max/f+1));
+    results_f(k, 1).Vs_phase = Vs_phase(1:int32(cycles*h_max/f+1));
+    results_f(k, 1).t_start = t_start;
+    results_f(k, 1).cycles = cycles;
+    results_f(k, 1).Fs = T;
+    results_f(k, 1).H_max = h_max;
+
+    % Plot in time and frequency domain
+    h_plot_max = 21;  % plot harmonics until
+
+    subplot(2,2,1)
+    plot(time, Vs)
+    title('Supply voltage, time domain')
+    xlabel('t (sec)')
+    ylabel('Voltage V(t)')
+
+    subplot(2,2,2)
+    bar(H/50, Vs_spec)
+    xlim([0, h_plot_max+1])
+    xticks((1:2:h_plot_max))
+    title('Supply voltage, frequency domain')
+    xlabel('Harmonic (fund. = 50 Hz)')
+    ylabel('Voltage V(f)')
+
+    subplot(2,2,3)
+    plot(time, Is)
+    title('Current injection, time domain')
+    xlabel('t (sec)')
+    ylabel('Current I(t)')
+
+    subplot(2,2,4)
+    bar(H/50, I_inj)
+    xlim([0, h_plot_max+1])
+    xticks((1:2:h_plot_max))
+    title('Current injection, frequency domain')
+    xlabel('Harmonic (fund. = 50 Hz)')
+    ylabel('Current I(f)')
+end
+
+% simulate with both voltage sources, fundamental source is constant
 for i = (1:length(supply_harmonics))
     for j = (1:length(supply_voltage_h))
         fh = supply_harmonics(i);
         Vh = supply_voltage_h(j);
+        Initialph_f = 0;
 
         % execute circuit simulation
         sim("SMPS")
-
-        t_end = t_start + cycles/f - T;
-        time_complete = (0:T:t);
-        time = (t_start : T : t_end);
-        L = length(time);  % time-series length
-        H = (0:(L/2))/L/T;  % spectrum
 
         % Simulation output
         Is_complete = S_scope.signals(1).values(1:end);
@@ -53,7 +139,7 @@ for i = (1:length(supply_harmonics))
         Vs_complete = S_scope.signals(2).values(1:end);
         Vs = S_scope.signals(2).values(int32(t_start/T+1):int32(t_end/T+1));
 
-        % FFT
+        % FFT I source
         ft_i = fft(Is);
         % two-sided spectrum. abs is magnitute of frequency
         P2_i = abs(ft_i/L);  
@@ -63,6 +149,7 @@ for i = (1:length(supply_harmonics))
         % phase somehow is shifted by -90 degree (bc of inductive element?)
         I_inj_phase = angle(ft_i(1:L/2+1))+pi/2;
         
+        % FFT V source
         ft_v = fft(Vs);
         % two-sided spectrum. abs is magnitute of frequency
         P2_v = abs(ft_v/L);  
@@ -72,20 +159,20 @@ for i = (1:length(supply_harmonics))
         % phase somehow is shifted by -90 degree and interharmonics wrong
         Vs_phase = angle(ft_v(1:L/2+1))+pi/2;
 
-        % save paramters and results as struct
-        results(i, j).V_m_f = Va;
-        results(i, j).V_m_h = Vh;
-        results(i, j).V_a_f = Initialph_f;
-        results(i, j).V_a_h = Initialph_h;
-        results(i, j).f_h = fh;
-        results(i, j).H = H(1:int32(cycles*h_max/f+1));
-        results(i, j).I_inj = I_inj(1:int32(cycles*h_max/f+1));
-        results(i, j).I_inj_phase = I_inj_phase(1:int32(cycles*h_max/f+1));
-        results(i, j).Vs_phase = Vs_phase(1:int32(cycles*h_max/f+1));
-        results(i, j).t_start = t_start;
-        results(i, j).cycles = cycles;
-        results(i, j).Fs = T;
-        results(i, j).H_max = h_max;
+        % save paramters and results of harmonic sims in struct
+        results_h(i, j).V_m_f = Va;
+        results_h(i, j).V_m_h = Vh;
+        results_h(i, j).V_a_f = Initialph_f;
+        results_h(i, j).V_a_h = Initialph_h;
+        results_h(i, j).f_h = fh;
+        results_h(i, j).H = H(1:int32(cycles*h_max/f+1));
+        results_h(i, j).I_inj = I_inj(1:int32(cycles*h_max/f+1));
+        results_h(i, j).I_inj_phase = I_inj_phase(1:int32(cycles*h_max/f+1));
+        results_h(i, j).Vs_phase = Vs_phase(1:int32(cycles*h_max/f+1));
+        results_h(i, j).t_start = t_start;
+        results_h(i, j).cycles = cycles;
+        results_h(i, j).Fs = T;
+        results_h(i, j).H_max = h_max;
 
         % Plot in time and frequency domain
         h_plot_max = 21;  % plot harmonics until
@@ -121,4 +208,5 @@ for i = (1:length(supply_harmonics))
 end
 
 % export results
-save('circuit_sim.mat', 'results');
+all = struct("results_f", results_f, "results_h", results_h);
+save('circuit_sim.mat', 'all');
