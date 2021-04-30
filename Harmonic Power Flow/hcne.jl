885532a83e68d63b7af6d4979a7783f1c55697ca
Pascal Weigmann <p.weigmann@posteo.de>
2021-04-30 12:29:59 +0200

    translated fundamental power flow, still buggy

--- Harmonic Power Flow/hcne.jl
+++ Harmonic Power Flow/hcne.jl
@@ -3,6 +3,16 @@ using OrderedCollections: OrderedDict
 using CSV
 using SparseArrays
 
+"""
+requires Julia 1.6 (spdiagm)
+
+notation
+u: complex voltage
+v: voltage magnitude
+ϕ: voltage phase
+
+"""
+
 # global variables
 BASE_POWER = 1000  # could also be be imported with infra, as nominal sys power
 BASE_VOLTAGE = 230
@@ -10,7 +20,7 @@ H_MAX = 5
 HARMONICS = [h for h in 1:2:H_MAX]
 NET_FREQ = 50
 HARMONICS_FREQ = [NET_FREQ * i for i in HARMONICS]
-PATH = "harmonic-power-flow\\Harmonic Power Flow\\"
+NET_PATH = "harmonic-power-flow\\Harmonic Power Flow\\"
 
 # pu system derived base values
 base_current = 1000*BASE_POWER/BASE_VOLTAGE
@@ -56,8 +66,8 @@ function init_network(name, manually=false)
         nodes = import_nodes_manually()
         lines = import_lines_manually()
     else
-        nodes = import_nodes_from_csv(PATH*name)
-        lines = import_lines_from_csv(PATH*name)
+        nodes = import_nodes_from_csv(NET_PATH*name)
+        lines = import_lines_from_csv(NET_PATH*name)
     end
     # find first nonlinear bus
     m = minimum(nodes[nodes[:,"type"] .== "nonlinear",:].ID)
@@ -69,12 +79,12 @@ end
 """
     admittance_matrices(nodes, lines, harmonics)
 
-Build the nodal admittance matrices (admittance laplacian) for all harmonics.
+Build the nodal admittance matrices (admittance laplacian) for all harmonics. Admittance scales linearly with frequency.
 """
 function admittance_matrices(nodes, lines, harmonics)
     LY = Dict()
     for h in harmonics
-        LY[h] = spzeros(Complex, n,n)
+        LY[h] = spzeros(ComplexF64, n,n)
         # non-diagonal elements
         for line in eachrow(lines)
             LY[h][line.fromID, line.toID] = -1/(line.R + 1im*line.X*h)
@@ -113,28 +123,101 @@ function init_voltages(nodes, harmonics, v1=1, ϕ1 = 0, vh=0.1, ϕh=0)
 end
 
 
-function init_fund_state_vec(u)
-    xv = u[1].v
-    xϕ = u[1].ϕ
-    for h in HARMONICS[2:end]
-        xv = vcat(xv, u[h].v)
-        xϕ = vcat(xϕ, u[h].ϕ)
-    end
-    vcat(xv, xϕ)
+function fund_state_vec(u)
+    xv = u[1].v[2:end]
+    xϕ = u[1].ϕ[2:end]
+    vcat(xv, xϕ)  # note: magnitude first
 end
 
 
-function fund_mismatch(nodes, u, LY_1)
-    u1 = u[1].v .* exp.(1im*u[1].ϕ)
+function fund_mismatch(nodes, u, LY)
+    LY_1 = LY[1]
+    u_1 = u[1].v .* exp.(1im*u[1].ϕ)
     s = (nodes.P + 1im*nodes.Q)/BASE_POWER
-    mismatch = u1 .* conj(LY_1*u1) + s
+    mismatch = u_1 .* conj(LY_1*u_1) + s
     f = vcat(real(mismatch[2:end]), imag(mismatch[2:end]))
     err = maximum(f)
     f, err
 end
 
+
+function fund_jacobian(u, LY)
+    LY_1 = LY[1]
+    u_1 = u[1].v .* exp.(1im*u[1].ϕ)
+    i_diag = spdiagm(sparse(LY_1 * u_1))
+    u_1_diag = spdiagm(u_1)
+    u_1_diag_norm = spdiagm(u_1./u_1)
+
+    dSdϕ = 1im*u_1_diag*(i_diag - LY_1 * u_1_diag)'
+    dSdv = u_1_diag_norm * i_diag' + u_1_diag * (LY_1 * u_1_diag_norm)'
+
+    # divide sub-matrices into real and imag part, cut off slack
+    dPdϕ = real(dSdϕ[2:end, 2:end])
+    dPdv = real(dSdv[2:end, 2:end])
+    dQdϕ = imag(dSdϕ[2:end, 2:end])
+    dQdv = imag(dSdv[2:end, 2:end])
+
+    J_1 = vcat(hcat(dPdϕ, dPdv), 
+            hcat(dQdϕ, dQdv))
+end
+
+
+function update_fund_state_vec(J, x, f)
+    x_new = x - J_1\f  # Newton-Raphson iteration
+end
+
+
+function update_fund_voltages(u, x)
+    u[1].v[2:end] = x[1:(length(x)÷2)]
+    u[1].ϕ[2:end] = x[(length(x)÷2+1):end]
+    u
+end
+
+
+function pf(LY, nodes, thresh_f = 1e-6, max_iter_f = 30, 
+            plt_convergence = false)
+    u = init_voltages(nodes, HARMONICS)
+    x_f = fund_state_vec(u)
+    f_f, err_f = fund_mismatch(nodes, u, LY)
+
+    n_iter_f = 0
+    err_f_t = Dict()
+    while err_f > thresh_f && n_iter_f <= max_iter_f
+        J_f = fund_jacobian(u, LY)
+        x_f = update_fund_state_vec(J_f, x_f, f_f)
+        u = update_fund_voltages(u, x_f)
+        f_f, err_f = fund_mismatch(nodes, u, LY)
+        err_f_t[n_iter_f] = err_f
+        n_iter_f += 1
+    end
+
+    
+
+    if n_iter_f < max_iter_f
+        print("Fundamental power flow converged after ", n_iter_f, 
+              " iterations.")
+    elseif n_iter_f == max_iter_f
+        print("Maximum of ", n_iter_f, " iterations reached.")
+    end
+    u, err_f_t, n_iter_f
+end
+
+
 nodes, lines, m, n = init_network("net2")
-LY = admittance_matrices(nodes, lines, HARMONICS)
-u = init_voltages(nodes, HARMONICS)
-x = init_fund_state_vec(u)
-f, err_f = fund_mismatch(nodes, u, LY[1])
+
+u, err_f_t, n_iter_f = pf(LY, nodes)
+# where is the bug
+
+
+
+
+
+function harmonic_state_vec(u)
+    xv = u[1].v[2:end]
+    xϕ = u[1].ϕ[2:end]
+    for h in HARMONICS[2:end]
+        xv = vcat(xv, u[h].v)
+        xϕ = vcat(xϕ, u[h].ϕ)
+    end
+    vcat(xv, xϕ)  # note: magnitude first
+end
