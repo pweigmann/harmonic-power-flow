fixed normalization while deriving current injections

Circuit Simulation/NE_from_sim.py
@@ -12,178 +12,188 @@ from scipy.io import loadmat
 #  pass name of simulated device (other meta data as well?)
 
 # import from .mat file
-device_name = "smps"
-data = loadmat(device_name + '.mat', squeeze_me=True, struct_as_record=False)
-df = data["all"].results_f  # fundamental simulation results
-dh = data["all"].results_h  # harmonic simulation results
-
-''' Data structure - harmonic (dh) and fundamental (df)
-dh[a, b]: a is index for harmonic, varying harmonic voltage source frequency
-          b is index for measurement, varying harmonic voltage source magnitude
-          Minimum values for uncoupled script to work: a > 0, b > 0 
-          Example: dh[0, 1] means first harmonic frequency (f_h = 150), 
-                   second measurement (e.g. V_m_h = 23).
-df[c]:    c is index for measurement, varying fundamental voltage source angle
-'''
-
-# --- Data Cleaning ---
-# check imported data size
-if len(dh[0, :]) < 2:
-    raise ValueError('At least 2 measurements needed for script to work.')
-elif len(dh[:, 0]) < 2:
-    raise ValueError('At least 2 harmonics needed for script to work.')
-
-# convert to multi-index DataFrame
-supply_f = []
-for n in dh:
-    supply_f.append(n[0].f_h)
-
-supply_v_m = []
-for m in dh[0, :]:
-    supply_v_m.append(m.V_m_h)
-
-# this will only work for constant harmonic supply voltage angles
-supply_v_a = [dh[0, 0].V_a_h]
-
-iterables = [supply_v_m, supply_f, supply_v_a]
-multi_idx = pd.MultiIndex.from_product(iterables, names=['supply_v_m',
-                                                         'supply_f',
-                                                         'supply_v_a'])
-spectrum = pd.Int64Index(dh[0, 0].H.astype(int), name="Frequency",
-                         dtype="int64")
-
-# initialize multi-index DataFrame
-I_inj_complete = pd.DataFrame(np.zeros((len(supply_f)*len(supply_v_m),
-                                        len(dh[0, 0].H))), index=multi_idx,
-                              columns=spectrum)
-V_supply = pd.DataFrame(np.zeros((len(supply_f)*len(supply_v_m), 1)),
-                        index=multi_idx)
-# iterate through harmonic measurements
-for i in dh:
-    for j in i:
-        # write I_inj of corresponding voltage to DataFrame
-        I_inj_complete.loc[(j.V_m_h, j.f_h, j.V_a_h)] = \
-            j.I_inj*np.exp(1j*j.I_inj_phase)
-        V_supply.loc[(j.V_m_h, j.f_h, j.V_a_h)] = \
-            j.V_m_h*np.exp(1j*j.V_a_h*np.pi/180)
-
-# iterate through fundamental measurements
-for s in df:
-    # write I_inj of corresponding voltage to DataFrame, f_fund always 50 Hz
-    I_inj_complete.loc[(s.V_m_f, 50, s.V_a_f)] = \
-        s.I_inj*np.exp(1j*s.I_inj_phase)
-
-# final I_inj for all measurements, no inter-harmonics
-I_inj = I_inj_complete.loc(axis=1)[50::dh[0, 0].cycles*2]
+device_name = "SMPS"
+f_max_settings = [250, 1050, 2550, 5050]
 
 
-# --- calculate Norton Equivalent parameters, UNCOUPLED ---
-# uncoupled (see Thunberg.1999), 2*n measurements (of 1 freq.) for n harmonics
-# build difference between the two measurements, without fund, m2 - m1
-dI_h = (I_inj.loc[dh[0, 1].V_m_h] - I_inj.loc[dh[0, 0].V_m_h]).drop(
-    50, axis=1)
-# voltage difference is constant wrt frequency
-V_h_m1 = dh[0, 0].V_m_h*np.exp(1j*dh[0, 0].V_a_h*np.pi/180)
-V_h_m2 = dh[0, 1].V_m_h*np.exp(1j*dh[0, 1].V_a_h*np.pi/180)
-# only diagonal elements needed for harmonic frequencies
-# Norton admittance, harmonic
-Y_N_h = pd.Series(np.diag(dI_h)/(V_h_m1 - V_h_m2), index=supply_f)
-Y_N_h.rename_axis(index="Frequency", inplace=True)
-# Norton current source, harmonic:
-I_N_h = Y_N_h*V_h_m1 + np.diag(I_inj.loc[dh[0, 0].V_m_h, 150:])
+def get_NE_from_sim(device_name, f_max):
+    ''' Data structure - harmonic (dh) and fundamental (df)
+    dh[a, b]: a is index for harmonic, varying harmonic voltage source frequency
+              b is index for measurement, varying harmonic voltage source magnitude
+              Minimum values for uncoupled script to work: a > 0, b > 0
+              Example: dh[0, 1] means first harmonic frequency (f_h = 150),
+                       second measurement (e.g. V_m_h = 23).
+    df[c]:    c is index for measurement, varying fundamental voltage source angle
+    '''
+    data = loadmat(device_name + "_" + str(f_max) + ".mat",
+                   squeeze_me=True, struct_as_record=False)
+    df = data["all"].results_f  # fundamental simulation results
+    dh = data["all"].results_h  # harmonic simulation results
 
 
-# build difference between the two measurements, only fund, m2 - m1
-dI_f = I_inj.loc[(df[1].V_m_f, 50, df[1].V_a_f)] - \
-       I_inj.loc[(df[0].V_m_f, 50, df[0].V_a_f)]
-# voltage difference
-V_f_m1 = df[0].V_m_f*np.exp(1j*df[0].V_a_f*np.pi/180)
-V_f_m2 = df[1].V_m_f*np.exp(1j*df[1].V_a_f*np.pi/180)
-# only fund injection needed for fundamental frequency
-Y_N_f = dI_f[[50]]/(V_f_m1 - V_f_m2)  # Norton admittance, fundamental
-# Norton current source, fundamental:
-I_N_f = Y_N_f*V_f_m1 + I_inj.loc[(df[0].V_m_f, 50, df[0].V_a_f), [50]]
 
-# Final Norton parameters, uncoupled model:
-I_N_uc = I_N_f.append(I_N_h)
-Y_N_uc = Y_N_f.append(Y_N_h)
+    # --- Data Cleaning ---
+    # check imported data size
+    if len(dh[0, :]) < 2:
+        raise ValueError('At least 2 measurements needed for script to work.')
+    elif len(dh[:, 0]) < 2:
+        raise ValueError('At least 2 harmonics needed for script to work.')
 
-# test uncoupled (using measurement 1)
-# calculate I_inj with NE
-# I_inj_test = I_N_uc - np.squeeze(np.diag(Y_N_uc).dot(pd.Series(V_f_m1).append(
-#     V_supply.loc[dh[0, 0].V_m_h])))
-# take I_inj from circuit sim
-# I_inj_m1 = pd.Series(I_inj.loc[(df[0].V_m_f, 50, df[0].V_a_f), [50]]).append(
-#     pd.Series(np.diagonal(I_inj.loc[dh[0, 0].V_m_h], 1), index=supply_f))
-# err = I_inj_test - I_inj_m1
-# test (using measurement 2)
-# I_inj_test_2 = I_N_uc - np.squeeze(np.diag(Y_N_uc).dot(pd.Series(V_f_m2).append(
-#     V_supply.loc[dh[0, 1].V_m_h])))
-# I_inj_m2 = pd.Series(I_inj.loc[(df[1].V_m_f, 50, df[1].V_a_f), [50]]).append(
-#     pd.Series(np.diagonal(I_inj.loc[dh[0, 1].V_m_h], 1), index=supply_f))
-# err2 = I_inj_test_2 - I_inj_m2
-#
-# if np.linalg.norm((err, err2), np.inf) > 1e-6:
-#     print("Warning: uncoupled NE test failed!")
+    # convert to multi-index DataFrame
+    supply_f = []
+    for n in dh:
+        supply_f.append(n[0].f_h)
+
+    supply_v_m = []
+    for m in dh[0, :]:
+        supply_v_m.append(m.V_m_h)
+
+    # this will only work for constant harmonic supply voltage angles
+    supply_v_a = [dh[0, 0].V_a_h]
+
+    iterables = [supply_v_m, supply_f, supply_v_a]
+    multi_idx = pd.MultiIndex.from_product(iterables, names=['supply_v_m',
+                                                             'supply_f',
+                                                             'supply_v_a'])
+    spectrum = pd.Int64Index(dh[0, 0].H.astype(int), name="Frequency",
+                             dtype="int64")
+
+    # initialize multi-index DataFrame
+    I_inj_complete = pd.DataFrame(np.zeros((len(supply_f)*len(supply_v_m),
+                                            len(dh[0, 0].H))), index=multi_idx,
+                                  columns=spectrum)
+    V_supply = pd.DataFrame(np.zeros((len(supply_f)*len(supply_v_m), 1)),
+                            index=multi_idx)
+    # iterate through harmonic measurements
+    for i in dh:
+        for j in i:
+            # write I_inj of corresponding voltage to DataFrame
+            I_inj_complete.loc[(j.V_m_h, j.f_h, j.V_a_h)] = \
+                j.I_inj*np.exp(1j*j.I_inj_phase)
+            V_supply.loc[(j.V_m_h, j.f_h, j.V_a_h)] = \
+                j.V_m_h*np.exp(1j*j.V_a_h*np.pi/180)
+
+    # iterate through fundamental measurements
+    for s in df:
+        # write I_inj of corresponding voltage to DataFrame, f_fund always 50 Hz
+        I_inj_complete.loc[(s.V_m_f, 50, s.V_a_f)] = \
+            s.I_inj*np.exp(1j*s.I_inj_phase)
+
+    # final I_inj for all measurements, no inter-harmonics
+    I_inj = I_inj_complete.loc(axis=1)[50::dh[0, 0].cycles*2]
 
 
-# --- calculate Norton Equivalent parameters, COUPLED ---
-# coupled (see Almeida.2010), n+1 measurements (of all freq.) for n harmonics
-# only one value for V_m_h needed (aka. one "measurement" of uncoupled NE)
-freq = [50] + supply_f  # all frequencies used as supply voltages
-N = len(freq)  # number of frequencies of which NE can be calculated
+    # --- calculate Norton Equivalent parameters, UNCOUPLED ---
+    # uncoupled (see Thunberg.1999), 2*n measurements (of 1 freq.) for n harmonics
+    # build difference between the two measurements, without fund, m2 - m1
+    dI_h = (I_inj.loc[dh[0, 1].V_m_h] - I_inj.loc[dh[0, 0].V_m_h]).drop(
+        50, axis=1)
+    # voltage difference is constant wrt frequency
+    V_h_m1 = dh[0, 0].V_m_h*np.exp(1j*dh[0, 0].V_a_h*np.pi/180)
+    V_h_m2 = dh[0, 1].V_m_h*np.exp(1j*dh[0, 1].V_a_h*np.pi/180)
+    # only diagonal elements needed for harmonic frequencies
+    # Norton admittance, harmonic
+    Y_N_h = pd.Series(np.diag(dI_h)/(V_h_m1 - V_h_m2), index=supply_f)
+    Y_N_h.rename_axis(index="Frequency", inplace=True)
+    # Norton current source, harmonic:
+    I_N_h = Y_N_h*V_h_m1 + np.diag(I_inj.loc[dh[0, 0].V_m_h, 150:])
 
-# constant V matrix for calculating coupled NEs
-V_mes = pd.DataFrame(np.zeros((N+1, N)), index=freq + [50], columns=freq)
-V_mes["I"] = -np.ones((N+1, 1))
-V_mes[50] = V_f_m1  # fundamental voltage constant throughout measurements...
-# ...except for N+1th. Second measurement at fundamental frequency placed at end
-V_mes.iloc[-1, 0] = V_f_m2
-# fill in harmonic supply voltages diagonally
-for h in supply_f:
-    V_mes.loc[h, h] = V_supply.loc[(supply_v_m[0], h, supply_v_a[0]), 0]
-V_mes = -V_mes
-V_inv = np.linalg.inv(V_mes)
+    # build difference between the two measurements, only fund, m2 - m1
+    dI_f = I_inj.loc[(df[1].V_m_f, 50, df[1].V_a_f)] - \
+           I_inj.loc[(df[0].V_m_f, 50, df[0].V_a_f)]
+    # voltage difference
+    V_f_m1 = df[0].V_m_f*np.exp(1j*df[0].V_a_f*np.pi/180)
+    V_f_m2 = df[1].V_m_f*np.exp(1j*df[1].V_a_f*np.pi/180)
+    # only fund injection needed for fundamental frequency
+    Y_N_f = dI_f[[50]]/(V_f_m1 - V_f_m2)  # Norton admittance, fundamental
+    # Norton current source, fundamental:
+    I_N_f = Y_N_f*V_f_m1 + I_inj.loc[(df[0].V_m_f, 50, df[0].V_a_f), [50]]
 
-# DataFrame to store results
-YI_N = pd.DataFrame(np.zeros((N+1, N)), index=freq + ["I"], columns=freq)
+    # Final Norton parameters, uncoupled model:
+    I_N_uc = I_N_f.append(I_N_h)
+    Y_N_uc = Y_N_f.append(Y_N_h)
 
-# sort and slice I_inj, so the columns can be used directly
-# first fund. I_inj
-d1 = I_inj.loc[[(df[0].V_m_f, 50, df[0].V_a_f)], freq]
-# harmonic I_inj
-d2 = I_inj.loc[(dh[0, 0].V_m_h, slice(None), dh[0, 0].V_a_h), freq]
-# second fund. I_inj
-d3 = I_inj.loc[[(df[1].V_m_f, 50, df[1].V_a_f)], freq]
-# concatenate DataFrames in correct order
-I_inj_c = d1.append([d2, d3])
-# evaluate NE, Y_N are added transposed
-YI_N[freq] = V_inv.dot(I_inj_c[freq])
+    # test uncoupled (using measurement 1)
+    # calculate I_inj with NE
+    # I_inj_test = I_N_uc - np.squeeze(np.diag(Y_N_uc).dot(pd.Series(V_f_m1).append(
+    #     V_supply.loc[dh[0, 0].V_m_h])))
+    # take I_inj from circuit sim
+    # I_inj_m1 = pd.Series(I_inj.loc[(df[0].V_m_f, 50, df[0].V_a_f), [50]]).append(
+    #     pd.Series(np.diagonal(I_inj.loc[dh[0, 0].V_m_h], 1), index=supply_f))
+    # err = I_inj_test - I_inj_m1
+    # test (using measurement 2)
+    # I_inj_test_2 = I_N_uc - np.squeeze(np.diag(Y_N_uc).dot(pd.Series(V_f_m2).append(
+    #     V_supply.loc[dh[0, 1].V_m_h])))
+    # I_inj_m2 = pd.Series(I_inj.loc[(df[1].V_m_f, 50, df[1].V_a_f), [50]]).append(
+    #     pd.Series(np.diagonal(I_inj.loc[dh[0, 1].V_m_h], 1), index=supply_f))
+    # err2 = I_inj_test_2 - I_inj_m2
+    #
+    # if np.linalg.norm((err, err2), np.inf) > 1e-6:
+    #     print("Warning: uncoupled NE test failed!")
 
-# final coupled Norton Equivalents, transpose Y_N back
-Y_N_c = np.transpose(YI_N.iloc[:-1])  # (Y_N_ff equal to uncoupled)
-I_N_c = YI_N.iloc[-1]  # (fundamental current source equal to uncoupled)
 
-# test coupled NE for h = 5
-V_uc_test5 = np.zeros(len(freq), dtype=complex)
-V_uc_test5[0] = V_f_m1
-V_uc_test5[2] = V_h_m1
-I_inj_c_test5 = I_N_c - Y_N_c.dot(V_uc_test5)  # calculate I_inj with NE
-err_c = I_inj_c_test5 - I_inj.loc[(dh[1, 0].V_m_h, dh[1, 0].f_h,
-                                   dh[1, 0].V_a_h)]
-if np.linalg.norm(err_c, np.inf) > 1e-6:
-    print("Warning: coupled NE test failed!")
+    # --- calculate Norton Equivalent parameters, COUPLED ---
+    # coupled (see Almeida.2010), n+1 measurements (of all freq.) for n harmonics
+    # only one value for V_m_h needed (aka. one "measurement" of uncoupled NE)
+    freq = [50] + supply_f  # all frequencies used as supply voltages
+    N = len(freq)  # number of frequencies of which NE can be calculated
 
-# export to file
-multi_idx_exp = pd.MultiIndex.from_arrays([
-    len(freq)*["Y_N_c"] + ["I_N_c", "Y_N_uc", "I_N_uc"],
-    freq + [0, 0, 0]], names=["Parameter", "Frequency"])
-NE = pd.DataFrame(np.zeros((len(freq)+3, len(freq))),
-                  index=multi_idx_exp, dtype=complex, columns=freq)
-NE.loc["Y_N_c"] = Y_N_c.to_numpy()
-NE.loc["I_N_c", 0] = I_N_c.values
-NE.loc["Y_N_uc", 0] = Y_N_uc.values
-NE.loc["I_N_uc", 0] = I_N_uc.values
+    # constant V matrix for calculating coupled NEs
+    V_mes = pd.DataFrame(np.zeros((N+1, N)), index=freq + [50], columns=freq)
+    V_mes["I"] = -np.ones((N+1, 1))
+    V_mes[50] = V_f_m1  # fundamental voltage constant throughout measurements...
+    # ...except for N+1th. Second measurement at fundamental frequency placed at end
+    V_mes.iloc[-1, 0] = V_f_m2
+    # fill in harmonic supply voltages diagonally
+    for h in supply_f:
+        V_mes.loc[h, h] = V_supply.loc[(supply_v_m[0], h, supply_v_a[0]), 0]
+    V_mes = -V_mes
+    V_inv = np.linalg.inv(V_mes)
 
-# NE.to_csv(device_name + "_NE.csv")
-# print("Created file " + device_name + "_NE.csv")
+    # DataFrame to store results
+    YI_N = pd.DataFrame(np.zeros((N+1, N)), index=freq + ["I"], columns=freq)
+
+    # sort and slice I_inj, so the columns can be used directly
+    # first fund. I_inj
+    d1 = I_inj.loc[[(df[0].V_m_f, 50, df[0].V_a_f)], freq]
+    # harmonic I_inj
+    d2 = I_inj.loc[(dh[0, 0].V_m_h, slice(None), dh[0, 0].V_a_h), freq]
+    # second fund. I_inj
+    d3 = I_inj.loc[[(df[1].V_m_f, 50, df[1].V_a_f)], freq]
+    # concatenate DataFrames in correct order
+    I_inj_c = d1.append([d2, d3])
+    # evaluate NE, Y_N are added transposed
+    YI_N[freq] = V_inv.dot(I_inj_c[freq])
+
+    # final coupled Norton Equivalents, transpose Y_N back
+    Y_N_c = np.transpose(YI_N.iloc[:-1])  # (Y_N_ff equal to uncoupled)
+    I_N_c = YI_N.iloc[-1]  # (fundamental current source equal to uncoupled)
+
+    # test coupled NE for h = 5
+    V_uc_test5 = np.zeros(len(freq), dtype=complex)
+    V_uc_test5[0] = V_f_m1
+    V_uc_test5[2] = V_h_m1
+    I_inj_c_test5 = I_N_c - Y_N_c.dot(V_uc_test5)  # calculate I_inj with NE
+    err_c = I_inj_c_test5 - I_inj.loc[(dh[1, 0].V_m_h, dh[1, 0].f_h,
+                                       dh[1, 0].V_a_h)]
+    if np.linalg.norm(err_c, np.inf) > 1e-6:
+        print("Warning: coupled NE test failed!")
+
+    # export to file
+    multi_idx_exp = pd.MultiIndex.from_arrays([
+        len(freq)*["Y_N_c"] + ["I_N_c", "Y_N_uc", "I_N_uc"],
+        freq + [0, 0, 0]], names=["Parameter", "Frequency"])
+    NE = pd.DataFrame(np.zeros((len(freq)+3, len(freq))),
+                      index=multi_idx_exp, dtype=complex, columns=freq)
+    NE.loc["Y_N_c"] = Y_N_c.to_numpy()
+    NE.loc["I_N_c", 0] = I_N_c.values
+    NE.loc["Y_N_uc", 0] = Y_N_uc.values
+    NE.loc["I_N_uc", 0] = I_N_uc.values
+
+    filename = device_name + "_" + str(f_max) + "_NE.csv"
+    NE.to_csv(filename)
+    print("Created file " + filename)
+
+
+for f_max in f_max_settings:
+    get_NE_from_sim(device_name, f_max)

Harmonic Power Flow/hcne_generalized.py
@@ -31,7 +31,6 @@ from sys import getsizeof
 import time
 
 
-
 # change complex numbers from algebraic to polar form
 def P2A(radii, angles):
     return radii * np.exp(1j*angles)
@@ -158,7 +157,7 @@ def build_admittance_matrices(buses, lines, harmonics):
             for m in range(len(lines)):
                 # go through all lines, see if they are connected to bus n,
                 # if yes add shunt admittance of pi line to bus admittance
-                if lines.loc[m, "fromID"] == n or lines.loc[m, "toID"]:
+                if lines.loc[m, "fromID"] == n or lines.loc[m, "toID"] == n:
                     Y[n, n] = Y[n, n] + \
                               (lines.loc[m, "G"]+1j*h*lines.loc[m, "B"])/2
 
@@ -271,6 +270,7 @@ def pf(Y, buses, thresh_f = 1e-6, max_iter_f = 30, plt_convergence=False):
 def import_Norton_Equivalents(buses, coupled):
     """import Norton Equivalents from files, returns dict of I_N, Y_N pairs"""
     # TODO: import NEs from other sources or input manually
+    #  find a better way how to choose considered harmonics and correct file
     #  alternative format? (e.g. HDF5 instead of csv)
 
     NE = {}
@@ -278,6 +278,8 @@ def import_Norton_Equivalents(buses, coupled):
     for device in nl_components:
         file_path = str("~/Git/harmonic-power-flow/Circuit Simulation/"
                         + device + "_NE.csv")
+        #file_path = str("~/Git/harmonic-power-flow/Circuit Simulation/"
+        #        + device + "_" + str(max(HARMONICS_FREQ)) + "_NE.csv")
         NE_device = pd.read_csv(file_path, index_col=["Parameter", "Frequency"])
         # change column type from str to int
         NE_device.columns = NE_device.columns.astype(int)
@@ -408,7 +410,7 @@ def build_harmonic_jacobian(V, Y, NE, coupled):
     # indices of all nonlinear buses
     nl_idx_all = sum([list(range(nl, nl+n-m)) for nl in nl_idx_start], [])
     nl_V = V_vec.iloc[nl_idx_all]
-    nl_V_norm = nl_V/A2P(nl_V)[0]
+    nl_V_norm = nl_V/V.iloc[nl_idx_all, 0]
     # Fuchs didn't derive the current injections, so maybe I shouldn't either?
     if coupled:
         for h in range(n_blocks):  # iterating through blocks vertically
@@ -466,12 +468,25 @@ def update_harmonic_voltages(V, x):
     V.iloc[idx[1:], 0] = x[:int(len(x)/2)]
     V.iloc[idx[1:], 1] = x[int(len(x)/2):]
 
+    # modulo phase wrt 2pi (doesn't work)
+    #V["V_a"] = V["V_a"] % (2*np.pi)
+
+    # simulating julia behaviour (also doesn't seam to help)
+    # whenever phase is bigger than 2pi, normal modulo
+    #V.loc[V.V_a > 2*np.pi, "V_a"] = \
+    #    V.loc[V.V_a > 2*np.pi, "V_a"] % 2*np.pi
+    # whenever phase is smaller than -2pi, not normal modulo
+    #V.loc[V.V_a < -2*np.pi, "V_a"] = \
+    #    -1*(abs(V.loc[V.V_a < -2*np.pi, "V_a"]) % 2*np.pi)
+
     # avoid negative voltage magnitudes
     # add pi to negative voltage magnitudes
     # V.loc[V["V_m"] < 0, "V_a"] = V.loc[V["V_m"] < 0, "V_a"] - np.pi
     # V.loc[V["V_m"] < 0, "V_m"] = -V.loc[V["V_m"] < 0, "V_m"]  # change sign
     # -> this doesn't work, why? Instead only performed once in the end
-    # -> normalization will give false negative result if doing this
+    # --> normalization will give false negative result if doing this
+
+    # TODO: calculations (probably normalization) independent of phase modulo
 
     return V
 
@@ -492,12 +507,12 @@ def hpf(buses, lines, coupled, thresh_h=1e-4, max_iter_h=50,
     t_end_init = time.perf_counter()
     V, err1_t, n_converged = pf(Y, buses)
     t_end_pf = time.perf_counter()
+    # import all NE of nonlinear devices present in "buses"
     NE = import_Norton_Equivalents(buses, coupled)
     t_end_NE_import = time.perf_counter()
     n_iter_h = 0
     f, err_h = harmonic_mismatch(V, Y, buses, NE)
     x = harmonic_state_vector(V)
-    # import all NE of nonlinear devices present in "buses"
     err_h_t = {}
     global t_start_hpf_solve, t_end_hpf_solve
     t_start_hpf_solve = time.perf_counter()
@@ -545,10 +560,11 @@ t_start = time.perf_counter()
 # global variables
 BASE_POWER = 1000  # in W
 BASE_VOLTAGE = 230  # in V
-H_MAX = 10
-HARMONICS = [h for h in range(1, H_MAX+1, 2)]
-#HARMONICS = [1, 5]
+
+H_MAX = 5  # has to be either 5, 21, 51 or 101
+
 NET_FREQ = 50
+HARMONICS = [h for h in range(1, H_MAX+1, 2)]
 HARMONICS_FREQ = [NET_FREQ * i for i in HARMONICS]
 
 # helper definitions
@@ -560,15 +576,14 @@ base_admittance = base_current/BASE_VOLTAGE
 base_impedance = 1/base_admittance
 
 
-
 buses, lines, m, n = init_network("net2")
-Y = build_admittance_matrices(buses, lines, HARMONICS)
-V_f, err_f, n_converged_f = pf(Y, buses)
+#Y = build_admittance_matrices(buses, lines, HARMONICS)
+#V_f, err_f, n_converged_f = pf(Y, buses)
 
 
-# V_h, err_h_final, n_iter_h, J = hpf(buses, lines, coupled=True,
-#                                     plt_convergence=False)
-# THD_buses = get_THD(V_h)
+V_h, err_h_final, n_iter_h, J = hpf(buses, lines, coupled=True,
+                                    plt_convergence=False)
+THD_buses = get_THD(V_h)
 
 
 t_end = time.perf_counter()